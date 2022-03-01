df189fb850010423a5102bdce866746c38042f25
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-03-01 16:18:57 +0400

    partial deployment

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,30 @@
 class RecipesController < ApplicationController
+  def index
+
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :index
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:comments)
+  end
 end
