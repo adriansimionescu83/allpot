89659c52374bcf8ff897b6b287f719801173ec73
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-03-02 11:38:50 +0400

    recipes api and other updates

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,26 @@
-class RecipesController < ApplicationController
-  def index
+require "json"
+require "open-uri"
 
+class RecipesController < ApplicationController
+
+  def index
+    @recipes = policy_scope(Recipe)
+
+    api_key = ENV["SPOONTACULAR_API_KEY"]
+    puts api_key
+    url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=#{api_key}&ingredients=apples,+flour,+sugar&number=2"
+
+    recipes_serialized = URI.parse(url).read
+    recipes = JSON.parse(recipes_serialized)
+
+    recipes.each do |recipe|
+      puts recipe["id"]
+      puts recipe["title"]
+    end
   end
 
   def create
+
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
 
@@ -12,6 +29,8 @@ class RecipesController < ApplicationController
     else
       render :index
     end
+
+    authorize @recipe
   end
 
   def update
@@ -23,6 +42,19 @@ class RecipesController < ApplicationController
   end
 
   private
+  def call_spoontacular_api
+    api_key = ENV["SPOONTACULAR_API_KEY"]
+    puts api_key
+    url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=#{api_key}&ingredients=apples,+flour,+sugar&number=2"
+
+    recipes_serialized = URI.parse(url).read
+    recipes = JSON.parse(recipes_serialized)
+
+    recipes.each do |recipe|
+      new_recipe = Recipe.new(api_recipe_reference: recipe["id"])
+     end
+  end
+
 
   def recipe_params
     params.require(:recipe).permit(:comments)
