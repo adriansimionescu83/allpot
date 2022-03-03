7cc8637777e042c0018fbd909a3547484b026bab
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-03-03 12:40:58 +0400

    Fetching ingredients from the recipes

--- app/policies/recipe_policy.rb
+++ app/policies/recipe_policy.rb
@@ -6,4 +6,23 @@ class RecipePolicy < ApplicationPolicy
     end
   end
 
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def cooked?
+    true
+  end
 end
