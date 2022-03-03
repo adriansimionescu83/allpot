41c4825f5bc2f80d7ee4f49814e490701cb821ac
Shuja Khan <shuja3s@hotmail.com>
2022-03-03 12:36:09 +0400

    Modal Boostrap in progress

--- app/javascript/controllers/popup_controller.js
+++ app/javascript/controllers/popup_controller.js
@@ -1,11 +1,11 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["modal"]
+  // static targets = ["modal"]
 
-  revealContent() {
-    // this.contentTarget.classList.remove("d-none")
-    console.log("Hello, Stimulus!", this.element)
-    this.modalTarget.classList.remove("bg-modal-hidden")
-  }
+  // exampleModal() {
+  //   // this.contentTarget.classList.remove("d-none")
+  //   console.log("Hello, Stimulus!", this.element)
+  //   this.modalTarget.classList.remove("bg-modal-hidden")
+  // }
 }
