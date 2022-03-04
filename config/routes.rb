4e92a8d3cd5282eabb1cf36fddb4f4fcef72c1d0
Shuja Khan <shuja3s@hotmail.com>
2022-03-04 12:37:42 +0400

    Edit button and returning to the index after login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/user' => "recipes#index", :as => :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recipes, only: [:index, :show, :update]   #For now I have not added the cooked path because we might use index to show the cooked recipes
   resources :ingredients, only: %i[index create update destroy]
