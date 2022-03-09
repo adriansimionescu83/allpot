492577f173b459660a1e7ff30b6ba21e1562a591
Salam Baalbaki <salambaalbaki@gmail.com>
2022-03-09 12:55:53 +0400

    delete or add to shopping list from pantry

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recipes, only: [:index, :show, :update]
   resources :ingredients, only: %i[index create update destroy]
-  
+
   get 'recipes/:id/favorite', to: 'recipes#favorite', as: :favorites
 
   get 'recipes/:id/cooked', to: 'recipes#cooked'
 
   get 'my_recipes', to: 'recipes#my_recipes'
   patch 'shopping_list', to: 'ingredients#build_shopping_list', as: :build_shopping_list
+  patch 'ingredients/:id/add_to_shopping_list', to: 'ingredients#add_to_shopping_list', as: :add_to_shopping_list
+
   patch 'pantry', to: 'ingredients#move_to_pantry', as: :move_to_pantry
   get 'shopping_list', to: 'ingredients#shopping_list', as: :shopping_list
 
