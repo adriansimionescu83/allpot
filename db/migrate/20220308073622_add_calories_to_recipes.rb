56dccbdcbf899c660f91469a613a7417180b1468
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-03-10 11:57:32 +0400

    Revert "migration updates"
    This reverts commit 0c80d092eb03ec8274df9f3e00809b2755fb5462.

--- db/migrate/20220308073622_add_calories_to_recipes.rb
+++ db/migrate/20220308073622_add_calories_to_recipes.rb
@@ -1,8 +1,5 @@
 class AddCaloriesToRecipes < ActiveRecord::Migration[6.1]
   def change
     add_column :recipes, :calories, :string
-    add_column :recipes, :protein, :string
-    add_column :recipes, :carbs, :string
-    add_column :recipes, :fat, :string
   end
 end
