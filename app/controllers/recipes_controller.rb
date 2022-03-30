7af032ec23a32820ced5c9a6492e0fbe860fd053
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-03-30 21:47:53 +0400

    Enriched the schema for Recipes by adding new columns. Improved the API call to not delete the existing recipes but keep them in the database marked as non-recent results

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,11 +3,10 @@ require "open-uri"
 require 'date'
 
 class RecipesController < ApplicationController
-
   def index
-    get_ingredients
+    link_ingredients
     call_api if current_user.call_api_recipes
-    @recipes = policy_scope(Recipe).where(user_id: current_user.id).order(missed_ingredients_count: :asc)
+    @recipes = policy_scope(Recipe).where(user_id: current_user.id, is_latest_result: true).order(missed_ingredients_count: :asc)
     @user = current_user
 
     if params[:query].present?
@@ -18,19 +17,13 @@ class RecipesController < ApplicationController
   def favorites
     @recipe = Recipe.find(params[:id])
 
-    if @recipe.favorite == false
-     @recipe.favorite = true
-    else @recipe.favorite == true
-     @recipe.favorite = false
-    end
-
+    @recipe.favorite = @recipe.favorite.false?
     authorize @recipe
     if @recipe.save
       { errors: [] }.to_json
     else
       { errors: errors.messages }.to_json
     end
-
   end
 
   def my_recipes
@@ -38,7 +31,6 @@ class RecipesController < ApplicationController
     @favorite_recipes = policy_scope(Recipe).where(user_id: current_user.id, favorite: true)
     authorize @recipes
   end
-
 
   def cooked
     @recipe = Recipe.find(params[:id])
@@ -64,14 +56,14 @@ class RecipesController < ApplicationController
 
   private
 
-  def get_ingredients
+  def link_ingredients
     @ingredients = ''
-    available_ingredients = current_user.ingredients.select { |item|  item.is_available }
+    available_ingredients = current_user.ingredients.select { |item| item.is_available }
     available_ingredients.each do |ingredient|
       if ingredient == available_ingredients.last
         @ingredients += "#{ingredient.name.downcase}"
       else
-       @ingredients += "#{ingredient.name.downcase},"
+        @ingredients += "#{ingredient.name.downcase},"
       end
     end
   end
@@ -88,48 +80,41 @@ class RecipesController < ApplicationController
       intolerances = current_user.intolerances.join(',').downcase
     end
     ignore_pantry = 'false' #Whether to ignore typical pantry items, such as water, salt, flour, etc.
-    sort_by = 'min-missing-ingredients' #More options on sorting here https://spoonacular.com/food-api/docs#Recipe-Sorting-Options
-    sort_direction = 'asc'
+    sort_by = 'max-used-ingredients' #More options on sorting here https://spoonacular.com/food-api/docs#Recipe-Sorting-Options
+    sort_direction = 'desc'
     api_key = ENV["SPOONTACULAR_API_KEY"]
-    @url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=#{api_key}&number=10&includeIngredients=#{@ingredients}&addRecipeInformation=true&sort=#{sort_by}&sortDirection=#{sort_direction}&fillIngredients=true&diet=#{diet}&intolerances=#{intolerances}&ignorePantry=#{ignore_pantry}"
+    @url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=#{api_key}&number=100&includeIngredients=#{@ingredients}&addRecipeInformation=true&sort=#{sort_by}&sortDirection=#{sort_direction}&fillIngredients=true&diet=#{diet}&intolerances=#{intolerances}&ignorePantry=#{ignore_pantry}"
     recipes_serialized = URI.parse(@url).read
     recipes = JSON.parse(recipes_serialized)["results"]
-    if !recipes.empty?
-      Recipe.where(status: 'uncooked', favorite: false).destroy_all #removes all recipes that are not cooked so that these are replaced with the new matches found by API
-      Recipe.where(status: 'cooked').update(is_latest_result: false) #marks all existing recipes that are cooked as old records
-      # Recipe.where(favorite: true).update(is_latest_result: false) #marks all existing recipes that are favorites as old records
-    end
+    update_or_create_recipes(recipes) unless recipes.empty?
+    current_user.call_api_recipes = false
+    current_user.save
+    @last_call_time = DateTime.now
+  end
 
+  def update_or_create_recipes(recipes)
+    Recipe.where(is_latest_result: true).update(is_latest_result: false) # marks all existing recipes that as old records
     recipes.each do |recipe|
-      if Recipe.exists?(api_record_id: recipe["id"])
-        update_recipe_from_api(recipe)
+      existing_recipe = Recipe.where(api_recipe_reference: recipe["id"]).first
+      if existing_recipe
+        update_recipe_from_api(recipe, existing_recipe)
       else
         create_recipe_from_api(recipe)
       end
     end
-
-     current_user.call_api_recipes = false
-     current_user.save
-     @last_call_time = DateTime.now
   end
 
   def create_recipe_from_api(recipe)
     description_field = ""
     description_steps = []
-    if !recipe["analyzedInstructions"][0].nil?
-      recipe["analyzedInstructions"][0]["steps"].each do |step |
-        description_steps << "<b>Step #{step["number"]}:</b><br>#{step["step"]}"
-        description_field += "<b>Step #{step["number"]}:</b><br>#{step["step"]}"
+    unless recipe["analyzedInstructions"][0].nil?
+      recipe["analyzedInstructions"][0]["steps"].each do |step|
+        description_steps << "<b>Step #{step['number']}:</b><br>#{step['step']}"
+        description_field += "<b>Step #{step['number']}:</b><br>#{step['step']}"
       end
     end
-
-    # description_field = ""
-    # recipe["analyzedInstructions"][0]["steps"].each do |step |
-    #   description_field += "<b>Step #{step["number"]}:</b><br>#{step["step"]}<br><br>"
-    # end
-
-   new_recipe = Recipe.create(
-      api_record_id: recipe["id"],
+    Recipe.create(
+      api_recipe_reference: recipe["id"],
       status: 'uncooked',
       user_id: current_user.id,
       title: recipe["title"],
@@ -150,7 +135,21 @@ class RecipesController < ApplicationController
       aggregate_likes: recipe["aggregateLikes"],
       source_url: recipe["sourceUrl"],
       steps: description_steps,
-      diets: recipe["diets"]
+      diets: recipe["diets"],
+      vegetarian: recipe["vegetarian"],
+      vegan: recipe["vegan"],
+      gluten_free: recipe["glutenFree"],
+      dairy_free: recipe["dairyFree"],
+      ketogenic: recipe["ketogenic"],
+      spoonacular_score: recipe["spoonacularScore"],
+      credits_text: recipe["creditsText"],
+      source_name: recipe["sourceName"],
+      cuisines: recipe["cuisines"],
+      dish_types: recipe["dishTypes"],
+      occasions: recipe["occasions"],
+      spoonacular_source_url: recipe["spoonacularSourceUrl"],
+      likes: recipe["likes"],
+      intolerances: recipe["intolerances"]
       )
   end
 
@@ -165,7 +164,7 @@ class RecipesController < ApplicationController
   def total_ingredients(recipe)
     total_ingredients = []
     recipe["extendedIngredients"].each do |ingredient|
-      total_ingredients <<  "#{ingredient["measures"]["metric"]["amount"]} #{ingredient["measures"]["metric"]["unitShort"]} <b>#{ingredient["name"]}</b>"
+      total_ingredients <<  "#{ingredient['measures']['metric']['amount']} #{ingredient['measures']['metric']['unitShort']} <b>#{ingredient['name']}</b>"
     end
     return total_ingredients
   end
@@ -184,17 +183,9 @@ class RecipesController < ApplicationController
     recipe["missedIngredients"].map { |ingredient| ingredient["name"] }
   end
 
-  def update_recipe_from_api(recipe)
-    existing_recipe = Recipe.find_by api_record_id: recipe["id"]
-
+  def update_recipe_from_api(recipe, existing_recipe)
     existing_recipe.update(
-      missed_ingredients_count: recipe["missedIngredientCount"],
-      used_ingredients_count: recipe["usedIngredientCount"],
-      unused_ingredients_count: recipe["unusedIngredientCount"],
-      overall_score: recipe["overalScore"],
-      health_score: recipe["healthScore"],
       is_latest_result: true,
-      aggregate_likes: recipe["aggregateLikes"],
       missed_ingredients: missed_ingredients(recipe),
       unused_ingredients: unused_ingredients(recipe),
       used_ingredients: used_ingredients(recipe)
@@ -208,12 +199,12 @@ class RecipesController < ApplicationController
   def create_recipe_ingredients(recipe)
     current_user.ingredients.each do |pantry_ingredient|
       recipe.used_ingredients.each do |used_ingredient|
-       if used_ingredient.downcase.split(" ").any? {|ingredient| pantry_ingredient.name.include? ingredient} || pantry_ingredient.name.downcase.split(" ").any? {|ingredient| used_ingredient.include? ingredient}
-        RecipeIngredient.create(
+        if used_ingredient.downcase.split(" ").any? {|ingredient| pantry_ingredient.name.include? ingredient} || pantry_ingredient.name.downcase.split(" ").any? {|ingredient| used_ingredient.include? ingredient}
+          RecipeIngredient.create(
             ingredient_id: pantry_ingredient.id,
             recipe_id: recipe.id
           )
-       end
+        end
       end
     end
   end
